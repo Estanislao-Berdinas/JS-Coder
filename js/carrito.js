92ee5a1be9af16932df9e42fcc16de6174eedfbd
Estanislao-Berdinas <estanislaoberdinas@gmail.com>
2022-07-09 18:04:23 -0300

    trabajo finalizado

--- js/carrito.js
+++ js/carrito.js
@@ -3,7 +3,7 @@
 const numeroCarrito = () => {
 let compraParcial = localStorage.getItem('carrito')
 let carritoParcial = JSON.parse(compraParcial);
-const carritoNum = carritoParcial.length;
+const carritoNum = carritoParcial?.length || 0;
 console.log(carritoNum);
 
 document.getElementById ('contador').innerHTML=`
